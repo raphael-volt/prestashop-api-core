2e8bcb30ff82913202c24eeda7083a6971453b98
raphael_volt <raphael.ketmie@gmail.com>
2017-06-29 13:34:14 +0200

    bugfix import

--- src/core/abstract-resource.service.ts
+++ src/core/abstract-resource.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from "@angular/http";
 import { ApiConfigService } from "../api-config.service";
 import { AbstractResource } from "./abstract-resource";
-import { ResourceDescriptor } from "./resource-descrptor";
+import { ResourceDescriptor } from "./resource-descriptor";
 @Injectable()
 export class AbstractResourceService<T extends AbstractResource>  {
 
